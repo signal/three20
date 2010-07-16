62c2774d799a16581e66322066b6b9b2e5e42664
Doug Barth <dougbarth@gmail.com>
2010-07-16 16:35:31 -0500

    Deal with nil text values in UITextFields.

--- src/Three20UI/Sources/TTMessageController.m
+++ src/Three20UI/Sources/TTMessageController.m
@@ -675,10 +675,10 @@
     TTMessageField* field = [_fields objectAtIndex:fieldIndex];
     if ([field isKindOfClass:[TTMessageRecipientField class]]) {
       TTPickerTextField* pickerTextField = [_fieldViews objectAtIndex:fieldIndex];
-      return !pickerTextField.text.isEmptyOrWhitespace || pickerTextField.cellViews.count > 0;
+      return (pickerTextField.text && !pickerTextField.text.isEmptyOrWhitespace) || pickerTextField.cellViews.count > 0;
     } else {
       UITextField* textField = [_fieldViews objectAtIndex:fieldIndex];
-      return !textField.text.isEmptyOrWhitespace;
+      return textField.text && !textField.text.isEmptyOrWhitespace;
     }
   }
 }
